71a5de6a6513477c66a64a0abc833c58e19a630d
Paul O'Fallon <paul@ofallonfamily.com>
2011-11-20 23:28:14 -0500

    More account functions

--- test/account.js
+++ test/account.js
@@ -25,6 +25,7 @@ module.exports = testCase({
     nexmo.account.balance(function(err, b) {
       test.equals(err,null);
       test.notEqual(b,null);
+      test.equals(b > 0,true)
       test.done();
     });
 
@@ -35,6 +36,39 @@ module.exports = testCase({
     nexmo.account.pricing('US',function(err, p) {
       test.equals(err,null);
       test.notEqual(p,null);
+      test.done();
+    });
+
+  },
+
+  setSecret: function (test) {
+
+    nexmo.account.secret(testCredentials.secret, function(err, s) {
+      test.equals(err,null);
+      test.notEqual(s,null);
+      test.equal(s,testCredentials.secret);
+      test.done();
+    });
+
+  },
+
+  setInboundUrl: function (test) {
+
+    nexmo.account.inboundUrl('http://www.google.com', function(err, url) {
+      test.equals(err,null);
+      test.notEqual(url,null);
+      test.equals(url,'http://www.google.com');
+      test.done();
+    });
+
+  },
+
+  setReceiptsUrl: function (test) {
+
+    nexmo.account.receiptsUrl('http://www.google.com', function(err, url) {
+      test.equals(err,null);
+      test.notEqual(url,null);
+      test.equals(url,'http://www.google.com');
       test.done();
     });
 
